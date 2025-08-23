66fa26f296b39f8fe0af6f2cc234bdc8e2c58498
shaiksha <10syedshaiksha@gmail.com>
2025-08-23 23:56:26 +0530

    token handler with token emails

--- student_module/run.py
+++ student_module/run.py
@@ -2,8 +2,7 @@ from flask import Flask, request, render_template, jsonify,session,url_for,redir
 from code_engine import execute_code
 from flask_cors import CORS
 import time
-
-
+from token_handler import decrpyt_token
 app = Flask(__name__)
 
 CORS(app)
@@ -34,11 +33,27 @@ def index():
 @app.route('/index')
 def return_index():
     
-    name = session.get('name')
-    if name == None:
+    email = session.get('email')
+    question_id = session.get('question_id')
+    if email == None:
         return redirect(url_for('index'))
     
-    return render_template('index.html',name = name)
+    return render_template('index.html',name = email,question_id = question_id)
+
+@app.route('/test/<token>')
+def extract_data(token):
+    encrypted_token = token
+    value = decrpyt_token(encrypted_token)
+    print(value)
+    question_id = value['question_id']
+    email = value['email']
+    session['question_id'] = question_id
+    session['email'] = email
+
+    return redirect(url_for('return_index'))
+
+
+
 
 @app.route('/save_details',methods = ['POST'])
 def save_details():
@@ -61,15 +76,14 @@ def save_details():
 def run_code():
     try:
         data = request.get_json()
+        if not data:
+            return "Invalid request", 400
+        print(data)
         code = data.get("code", "")
         language = data.get("language", "python")
-        roll_no = session.get('roll_no')
 
-        length = len(roll_no) - 5
-        id = roll_no[length:]
-
+        id = session.get('email')
         output, error = execute_code(code, language,id)
-    
         return jsonify({"output": output, "error": error})
 
     except Exception as e:
